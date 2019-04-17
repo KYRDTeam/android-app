479aa4de293f16e19313fe0864597a3c8ebc0511
Steve Duong <steve@kyber.network>
2019-04-17 12:07:06 +0700

    Add custom fonts, implement landing page

--- app/src/main/java/com/kyberswap/android/presentation/base/BaseActivity.kt
+++ app/src/main/java/com/kyberswap/android/presentation/base/BaseActivity.kt
@@ -1,9 +1,16 @@
 package com.kyberswap.android.presentation.base
 
+import android.content.Context
 import android.support.v7.app.AppCompatDelegate
 import dagger.android.support.DaggerAppCompatActivity
+import io.github.inflationx.viewpump.ViewPumpContextWrapper
+
 
 abstract class BaseActivity : DaggerAppCompatActivity() {
+
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase))
+    }
 
     companion object {
         init {
--- app/src/main/java/com/kyberswap/android/presentation/landing/LandingFragment.kt
+++ app/src/main/java/com/kyberswap/android/presentation/landing/LandingFragment.kt
@@ -7,22 +7,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.kyberswap.android.R
+import com.kyberswap.android.databinding.FragmentLandingBinding
 
 private const val ARG_PARAM = "arg_param"
 
 
 class LandingFragment : Fragment() {
 
+    private lateinit var binding: FragmentLandingBinding
+
+    private var position: Int = 0
+
+    private val landingList by lazy {
+        listOf(
+                LandingViewModel(R.drawable.ic_security_check, R.string.landing_1_title, R.string.landing_1_content),
+                LandingViewModel(R.drawable.swap, R.string.landing_2_title, R.string.landing_2_content),
+                LandingViewModel(R.drawable.profile, R.string.landing_3_title, R.string.landing_3_content))
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        position = arguments?.getInt(ARG_PARAM) ?: 0
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_landing, container, false)
+        binding = FragmentLandingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding.viewModel = landingList[position]
     }
 
     companion object {
-        fun newInstance() =
+        fun newInstance(position: Int) =
                 LandingFragment().apply {
                     arguments = Bundle().apply {
-
+                        putInt(ARG_PARAM, position)
             
         
     }
