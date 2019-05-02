622e4e614da98ffdf2ef3dc79d1de687e2ae8be6
Steve Duong <steve@kyber.network>
2019-05-02 19:09:33 +0700

    Implement KyberSwap UI element

---
+++ app/src/main/java/com/kyberswap/android/presentation/main/swap/SwapFragment.kt
@@ -0,0 +1,67 @@
+package com.kyberswap.android.presentation.main.swap
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
+import com.kyberswap.android.AppExecutors
+import com.kyberswap.android.databinding.FragmentSwapBinding
+import com.kyberswap.android.domain.model.Wallet
+import com.kyberswap.android.presentation.base.BaseFragment
+import com.kyberswap.android.presentation.helper.Navigator
+import com.kyberswap.android.util.di.ViewModelFactory
+import javax.inject.Inject
+
+class SwapFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentSwapBinding
+
+    @Inject
+    lateinit var navigator: Navigator
+
+    @Inject
+    lateinit var appExecutors: AppExecutors
+
+    private var wallet: Wallet? = null
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+
+    private val viewModel by lazy {
+        ViewModelProviders.of(this, viewModelFactory).get(SwapViewModel::class.java)
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        wallet = arguments!!.getParcelable(WALLET_PARAM)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSwapBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding.wallet = wallet
+
+
+    }
+
+    companion object {
+        private const val WALLET_PARAM = "wallet_param"
+        fun newInstance(wallet: Wallet?) =
+            SwapFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelable(WALLET_PARAM, wallet)
+                }
+            }
+    }
+}
