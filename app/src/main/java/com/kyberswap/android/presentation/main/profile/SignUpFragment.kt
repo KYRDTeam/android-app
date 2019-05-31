71348a53f021d4d574ad978cd6554d798207ca23
Steve Duong <steve@kyber.network>
2019-05-31 15:06:59 +0700

    Implement profile feature

---
+++ app/src/main/java/com/kyberswap/android/presentation/main/profile/SignUpFragment.kt
@@ -0,0 +1,73 @@
+package com.kyberswap.android.presentation.main.profile
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.kyberswap.android.AppExecutors
+import com.kyberswap.android.databinding.FragmentProfileBinding
+import com.kyberswap.android.domain.SchedulerProvider
+import com.kyberswap.android.domain.model.Wallet
+import com.kyberswap.android.presentation.helper.Navigator
+import com.kyberswap.android.util.di.ViewModelFactory
+import javax.inject.Inject
+
+
+class SignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+
+    @Inject
+    lateinit var navigator: Navigator
+
+    @Inject
+    lateinit var appExecutors: AppExecutors
+
+    private var wallet: Wallet? = null
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    @Inject
+    lateinit var schedulerProvider: SchedulerProvider
+
+    private val viewModel by lazy {
+        ViewModelProviders.of(this, viewModelFactory).get(ProfileViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        wallet = arguments!!.getParcelable(WALLET_PARAM)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding.tvSignUp.setOnClickListener {
+            navigator.navigateToSignUpScreen()
+
+    }
+
+    companion object {
+        private const val WALLET_PARAM = "wallet_param"
+        fun newInstance(wallet: Wallet?) =
+            SignUpFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelable(WALLET_PARAM, wallet)
+        
+    
+    }
+
+
+}
