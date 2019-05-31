75ed965dbd4e1f39a0b422f227a7ba5932574b6c
Steve Duong <steve@kyber.network>
2019-05-31 15:06:59 +0700

    Implement profile feature

---
+++ app/src/main/java/com/kyberswap/android/presentation/main/profile/SignUpViewModel.kt
@@ -0,0 +1,9 @@
+package com.kyberswap.android.presentation.main.profile
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class SignUpViewModel @Inject constructor(
+) : ViewModel() {
+
+}
