1b28fb1caeeb35aed80d9f1dfa1dbbd4bb79f856
Steve Duong <steve@kyber.network>
2019-06-29 00:55:56 +0700

    Image picker from camera and gallery for personal info and passport

--- app/src/main/java/com/kyberswap/android/presentation/main/profile/kyc/PersonalInfoFragment.kt
+++ app/src/main/java/com/kyberswap/android/presentation/main/profile/kyc/PersonalInfoFragment.kt
@@ -1,19 +1,25 @@
 package com.kyberswap.android.presentation.main.profile.kyc
 
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
 import com.kyberswap.android.AppExecutors
+import com.kyberswap.android.R
 import com.kyberswap.android.databinding.FragmentPersonalInfoBinding
 import com.kyberswap.android.domain.SchedulerProvider
 import com.kyberswap.android.presentation.base.BaseFragment
 import com.kyberswap.android.presentation.helper.DialogHelper
 import com.kyberswap.android.presentation.helper.Navigator
 import com.kyberswap.android.util.di.ViewModelFactory
+import com.tbruyelle.rxpermissions2.RxPermissions
+import pl.aprilapps.easyphotopicker.*
 import javax.inject.Inject
 
 
@@ -37,6 +43,8 @@ class PersonalInfoFragment : BaseFragment() {
         Handler()
     }
 
+    private lateinit var easyImage: EasyImage
+
     @Inject
     lateinit var schedulerProvider: SchedulerProvider
 
@@ -56,11 +64,68 @@ class PersonalInfoFragment : BaseFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        val rxPermissions = RxPermissions(this)
         binding.tvNext.setOnClickListener {
             navigator.navigateToPassport(currentFragment)
         }
 
+        easyImage = EasyImage.Builder(this.context!!)
+            .setChooserTitle(getString(R.string.upload_document))
+            .setCopyImagesToPublicGalleryFolder(true)
+            .setChooserType(ChooserType.CAMERA_AND_GALLERY)
+            .setFolderName("kyc")
+            .build()
+        binding.tvUploadAddress.setOnClickListener {
+            dialogHelper.showImagePickerBottomSheetDialog(
+                {
+                    rxPermissions.request(Manifest.permission.CAMERA)
+                        .subscribe { granted ->
+                            if (granted) {
+                                easyImage.openCameraForImage(this)
+                            } else {
+                                showAlert(getString(R.string.permission_required))
+                            }
+                        }
+                },
+                {
+                    rxPermissions.request(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        .subscribe { granted ->
+                            if (granted) {
+                                easyImage.openGallery(this)
+                            } else {
+                                showAlert(getString(R.string.permission_required))
+                            }
+                        }
+                }
+            )
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        activity?.let {
+            easyImage.handleActivityResult(requestCode, resultCode, data, it, object :
+                DefaultCallback() {
+                override fun onMediaFilesPicked(imageFiles: Array<MediaFile>, source: MediaSource) {
+                    onPhotosReturned(imageFiles)
+                }
+
+                override fun onImagePickerError(error: Throwable, source: MediaSource) {
+
+                    error.printStackTrace()
+                }
+
+                override fun onCanceled(source: MediaSource) {
+
+                }
+            })
+        }
+    }
+
+    private fun onPhotosReturned(returnedPhotos: Array<MediaFile>) {
+        Glide.with(binding.imgAddress).load(returnedPhotos.first().file).into(binding.imgAddress)
+    }
+
 
     companion object {
         fun newInstance() =
